27c6406b0fde8141b58d11276b9665e0a3810f7b
Sayan <sayanpatra017@gmail.com>
2023-01-08 04:11:45 +0530

    !

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 let docTitle=document.title;
 window.addEventListener("blur", ()=>{document.title="Come back : ("});
 window.addEventListener("focus", ()=>{document.title=docTitle});
+
+
+const element1 = document.getElementsByClassName('panda1');
+
+element1.addEventListener('animationend', () => {
+  // start the second animation
+  element1.classList.add('backword');
+});
+const element = document.getElementById('panda2');
+
+element.addEventListener('animationstart', () => {
+  // start the second animation
+  element.classList.add('backword');
+});
