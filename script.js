df6869be00227bc39d1c42275878388b8d07ee36
Sayan <sayanpatra017@gmail.com>
2023-01-08 21:38:50 +0530

    0000

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ let docTitle=document.title;
 window.addEventListener("blur", ()=>{document.title="Come back : ("});
 window.addEventListener("focus", ()=>{document.title=docTitle});
 
+const left = document.getElementById("left-side");
 
+const handleMove = e => {
+  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
+}
+
+document.onmousemove = e => handleMove(e);
+
+document.ontouchmove = e => handleMove(e.touches[0]);
