51c36ea87be179dac72ba302faff63a903d8fd90
Sayan <sayanpatra017@gmail.com>
2023-01-08 16:04:59 +0530

    header complete

--- script.js
+++ script.js
@@ -3,15 +3,3 @@ window.addEventListener("blur", ()=>{document.title="Come back : ("});
 window.addEventListener("focus", ()=>{document.title=docTitle});
 
 
-const element1 = document.getElementsByClassName('panda1');
-
-element1.addEventListener('animationend', () => {
-  // start the second animation
-  element1.classList.add('backword');
-});
-const element = document.getElementById('panda2');
-
-element.addEventListener('animationstart', () => {
-  // start the second animation
-  element.classList.add('backword');
-});
