e1520cf121626b59888cc43d1f50233e9a755ab8
Sayan <sayanpatra017@gmail.com>
2023-01-17 21:08:38 +0530

    header

--- script.js
+++ script.js
@@ -29,4 +29,26 @@ const sayan2=document.getElementById("arrow3");
 sayan2.onclick=function(){
   document.getElementById("arrow2").scrollLeft-=510;
 }
+/*//////////////////////////////////////////*/
+const input = document.getElementById('ghost-input');
+const bubble = document.getElementById('ghost-bubble');
+
+const mouth = document.querySelector('.ghost__mouth');
+
+input.onkeydown = e => {
+  if (e.keyCode == 13) {
+    // bubble.innerText = e.target.value;
+    e.target.value = '';
+
+    mouthChatter
+    let i = 0;
+    if (mouthChatter) clearInterval(mouthChatter);
+
+    const mouthChatter = setInterval(() => {
+      mouth.classList.toggle('ghost__mouth--open');
+      if (i === 6) clearInterval(mouthChatter);
+      i++;
+    }, 300);
+  }
+};
 
